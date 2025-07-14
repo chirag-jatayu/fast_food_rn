2727e465cb8e7a38de6e068747e59657601fa91e
chirag-jatayu <chirag.jatayu@gmail.com>
2025-07-14 15:58:05 +0530

    Add databaseId and userId to Appwrite configuration

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.jsm.foodordering",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  databaseId: "6874d6e2000fbf54d22f",
+  userId: "6874d71500184aacc69b",
 };
