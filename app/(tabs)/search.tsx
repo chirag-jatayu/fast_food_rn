fb91ff8b0748712d4a7318d49af13b48bc0e88a6
chirag-jatayu <chirag.jatayu@gmail.com>
2025-07-14 13:40:38 +0530

    routing and appwrite setup

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Search = () => {
+  return (
+    <View>
+      <Text>search</Text>r
+    </View>
+  );
+};
+
+export default Search;
