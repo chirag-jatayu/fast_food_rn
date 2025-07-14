26cd2f4187d352b4502c3408752dcd8f5837574e
chirag-jatayu <chirag.jatayu@gmail.com>
2025-07-14 19:37:57 +0530

    done

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,14 +1,30 @@
+import { images } from "@/constants";
 import { Slot } from "expo-router";
 import React from "react";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  ImageBackground,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  View,
+} from "react-native";
 
 const _Layout = () => {
   return (
-    <SafeAreaView>
-      <Text>_layout</Text>
-      <Slot />
-    </SafeAreaView>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      <ScrollView
+        className="bg-white h-full"
+        keyboardShouldPersistTaps="handled"
+      >
+        <View>
+          <ImageBackground source={images.loginGraphic} />
+        </View>
+
+        <Slot />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
